5e34332239190c7aeb4516c91feb107c5e82bd10
조현서 <hyeonseojo929@gmail.com>
2025-06-06 20:03:21 +0900

    리스트 화면 구현

--- flask_server/app.py
+++ flask_server/app.py
@@ -4,7 +4,29 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)   
 
 # DB 대신 배열
-reservations = []
+reservations = [
+    {
+        'StudentNumber': '2115',
+        'date': '25/06/04',
+        'name': '조현서',
+        'isSelf': True,
+        'symptoms': '복통'
+    },
+    {
+        'StudentNumber': '2116',
+        'date': '25/06/05',
+        'name': '김철수',
+        'isSelf': False,
+        'symptoms': '두통'
+    },
+    {
+        'StudentNumber': '2117',
+        'date': '25/06/06',
+        'name': '박민지',
+        'isSelf': True,
+        'symptoms': '기침'
+    }
+]
 
 # # MySQL 데이터베이스 연결
 # app.config['SQLALCHEMY_DATABASE_URI'] = ''
@@ -25,13 +47,11 @@ def move():
 def information():
     if request.method == 'POST':
         
-        name = request.form['name']
-    
-        grade = request.form['grade']
-        ban = request.form['ban']
         number = request.form['number']
+        name = request.form['name']
+        password = request.form['password']
 
-        reservations.append({"name": name, "grade": grade, "ban": ban, "number": number,"isSelf" : None,"symptom" : None})
+        reservations.append({"name": name, "StudentNumber": number, "password" : password , "isSelf" : None,"symptom" : None, "check" : False})
         print(reservations)
         return render_template('cure_method.html')
     
@@ -70,6 +90,16 @@ def symptoms():
 @app.route('/final')
 def final():
     return render_template('final.html')
+
+@app.route('/list', methods=['GET', 'POST'])
+def list():
+
+    if request.method == 'POST':
+
+        return render_template('list.html')
+    
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1')
